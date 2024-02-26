c2f381f401cfb3ed39e068b316900f0d0b2c0be4
Deepak Ladha <ladhadeepak02@gmail.com>
2024-02-26 22:16:59 +0530

    frontend commit

--- Backend/server.js
+++ Backend/server.js
@@ -2,9 +2,28 @@ import express from 'express'
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
+
+app.get('/jokes',(req,res)=>{
+     const jokes = [
+        {
+            id:1,
+            title:'A joke'
+        },
+        {
+            id:2,
+            title:'A new joke'
+        },
+        {
+            id:3,
+            title:'A third joke'
+        }
 
 
+     ]
+
+     res.send(jokes)
+})
 app.listen(port,()=>{
     console.log(`server is listening`);
 })
